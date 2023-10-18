b3e820a9bba963e41e93cc6fe0a711249a0f4432
Houman <houman@houmanr.xyz>
2023-10-18 10:30:32 +0200

    Minor changes. Added langchain ollama caller

--- agent.py
+++ agent.py
@@ -17,41 +17,51 @@ class Agent:
         if login_response.status_code != 200:
             print("LOGIN FAILED")
 
-
     def process_message(self, frame):
         body = json.loads(frame.body)
         new_problem_id = body["problem"]["id"]
+        self.get_resolution_info(body)
 
         if self.problem_id != new_problem_id:
             self.get_problem_info(body)
-            self.get_resolution_info(body)
         else:
-            # it's probably? a chat message
+            # it's a chat message
             # CALL MODEL
-            print("CHAT MESSAGE?")
+            chat = body["chat"]
+            print(chat)
+            print()
+            print(self.problem_text)
+            print()
+            print(self.problem_notebook)
+            print()
+            print(self.problem_equations)
+            print()
+            print(self.problem_unknown_quantities)
+            print()
+            print(self.problem_known_quantities)
+            print()
+            print(self.problem_graphs)
 
     def get_problem_info(self, body):
         problem_id = body["problem"]["id"]
 
-        problem_response = self.session.get(
-            base_url + "/problems/" + str(problem_id)
-        )
+        problem_response = self.session.get(base_url + "/problems/" + str(problem_id))
 
         if problem_response.status_code != 200:
             print("ERROR GETTING PROBLEM DETAILS")
         else:
             self.problem_id = problem_id
             problem_json = json.loads(problem_response.text)
+            #self.problem_text = problem_json["text"]
             self.problem_graphs = problem_json["graphs"]
             self.problem_known_quantities = problem_json["knownQuantities"]
             self.problem_unknown_quantities = problem_json["unknownQuantities"]
-            
 
     def get_resolution_info(self, body):
-        self.problem_text = body["problem"]["text"] # This should be in get_problem_info but the endpoint returns null
+        # This should be in get_problem_info but the endpoint returns null
+        self.problem_text = body["problem"]["text"]
         self.problem_notebook = body["notebook"]
         self.problem_equations = body["equations"]
-
 
     def __str__(self):
         return self.room_id
---
+++ ollama.py
@@ -0,0 +1,55 @@
+from langchain.chat_models import ChatOllama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler                                  
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.schema import BaseOutputParser
+
+
+llm = ChatOllama(
+    #model="llama2:13b-chat",
+    #model="mistral-openorca",
+    model="llama2:7b-chat",
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]),
+    temperature=0,
+)
+
+
+class VarsExtractor(BaseOutputParser):
+    def parse(self, text: str):
+        lines = text.split("\n")
+        vars = [line.strip("*").strip() for line in lines if line.startswith("*")]
+        return vars
+
+
+get_known_vars_instructions = "Eres un asistente diseñado para extraer y enumerar las variables conocidas del problema proporcionado. No resuelva el problema, no agregue explicaciones o listas adicionales, no realice ningún cálculo y no proporcione ningún otro comentario. MUESTRA SÓLO LAS VARIABLES CONOCIDAS."
+get_known_vars_instructions = "Eres un asistente diseñado para extraer y enumerar las variables conocidas del problema proporcionado. No resuelva el problema, no agregue explicaciones o listas adicionales, no realice ningún cálculo y no proporcione ningún otro comentario. MUESTRA SÓLO LAS VARIABLES CONOCIDAS. Si el problema fuera 'Pedro y Juan han decidido vender limonadas en un día caluroso. Pedro vendió algunas limonadas a 2 euros cada una y Juan vendió las suyas a 3 euros cada una. Al final del día, Pedro y Juan habían vendido un total de 45 limonadas y habían recaudado 120 euros juntos. ¿Cuántas limonadas vendió cada uno?' deberías mostrar lo siguiente el formato '* Precio de limonadas de Pedro = 2\n* Precio de limonadas de Juan = 2'"
+
+get_unknown_vars_instructions = "Eres un asistente diseñado para extraer y enumerar las variables desconocidas del problema proporcionado. No resuelva el problema, no agregue explicaciones o listas adicionales, no realice ningún cálculo y no proporcione ningún otro comentario. MUESTRE SÓLO LAS VARIABLES DESCONOCIDAS SIN USAR NINGUNA INCÓGNITA."
+
+problem2 = "Pedro y Juan han decidido vender limonadas en un día caluroso. Pedro vendió algunas limonadas a 2 euros cada una y Juan vendió las suyas a 3 euros cada una. Al final del día, Pedro y Juan habían vendido un total de 45 limonadas y habían recaudado 120 euros juntos. ¿Cuántas limonadas vendió cada uno?"
+problem3 = "Lucía y Marta decidieron empezar un pequeño negocio vendiendo pulseras y collares durante el verano. El precio de cada pulsera es de 4 euros y el de cada collar es de 6 euros. En un día particular, vendieron un total de 25 ítems y recaudaron 110 euros. ¿Cuántas pulseras y cuántos collares vendieron?"
+problem4 = "En una tienda de mascotas, venden tres tipos diferentes de comida para animales: para perros, gatos y pájaros. Un día, se vendieron un total de 150 unidades entre las tres variedades. La comida para perros cuesta 3 euros la unidad, la comida para gatos cuesta 5 euros la unidad y la comida para pájaros cuesta 2 euros la unidad. Ese día, se recaudaron 500 euros en total. Si se sabe que se vendieron el mismo número de unidades de comida para perros y para gatos, ¿cuántas unidades de cada tipo de comida se vendieron?"
+problem1 = "Un grupo de estudiantes financia su viaje de fin de curso con la venta de participaciones de lotería, por importe de 1, 2 y 5 euros. Han recaudado, en total, 600 euros y han vendido el doble de participaciones de 1 euro que de 5 euros. Si han vendido un total de 260 participaciones, calcula el número de participaciones que han vendido de cada importe."
+problem = problem4
+
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "{instructions}"),
+    ("human", "HOLA QUE TAL"),
+    ("human", "{input}"),
+])
+
+#chain = prompt | llm | VarsExtractor()
+chain = prompt | llm
+
+# OpenAI can use abatch instead
+known_vars, unknown_vars = chain.batch([
+    {"instructions": get_known_vars_instructions, "input": problem},
+    {"instructions": get_unknown_vars_instructions, "input": problem}
+])
+
+print("KNOWN:")
+print(known_vars)
+print("\nUNKNOWN:")
+print(unknown_vars)
+
