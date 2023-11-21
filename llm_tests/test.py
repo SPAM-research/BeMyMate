2dc21b433daa0831a01e690155deeab9375020ab
Pablo Arnau-González <pablo.arnau@uv.es>
2023-11-21 19:02:22 +0100

    Implemented instructions array

--- llm_tests/test.py
+++ llm_tests/test.py
@@ -25,12 +25,15 @@ i1_5 = f"Given a problem and variable definitions, define one of the unknown var
 
 
 
-instructions = i1_1
-
-
-instructions2 = ""#i2_1
-instructions2 = i2_1
-instructions2 = i2_1
+instructions = [
+    f"Given a problem, variable and equations, define a variable {note_format} that hasn't been defined. Do not solve the problem. Your output must only be a variable definition {note_format}.",
+    f"Given a problem, variable and equations, define a variable {note_format} that hasn't been defined. Do not solve the problem. Your output must only be a variable definition {note_format}. You must output something that isn't in the input.",
+    f"Given a problem and some variables define a variable that hasn't been defined using the format {note_format}. Do not solve the problem. Output only the variable definition {note_format}.",
+    f"Define a variable that hasn't been defined using the format {note_format}. Do not solve the problem. Output only the variable definition {note_format}.",
+    f"Define a variable that hasn't been defined using the format {note_format} without solving the problem.",
+    f"Your task is to define a variable with the format '{note_format}' without solving the problem.",
+    f"Define a variable with the format '{note_format}' without solving the problem.",
+]
 
 
 
@@ -42,34 +45,48 @@ head_sep = "####################"
 up_sep = "vvvvvvvvvvvvvvvvvvvv"
 down_sep = "^^^^^^^^^^^^^^^^^^^^"
 all_outputs = []
-i = 1
+p = 1
 empty = 0
 
-ttf = 56
+ttf = 9
 problem_set = problems[ttf-1:ttf]
 problem_set = problems[:]
 total = len(problem_set)
 
 for problem in problem_set:
-    print(f"{i}/{total}")
-    [original, output] = test_problem(problem, instructions)
-    if output == "":
-        if instructions2 != "":
-            [original, output2] = test_problem(problem, instructions2)
-            if output2 == "":
-                empty += 1
-            else:
-                all_outputs.append(f"{i}\nT: {problem[0]}\nN: {problem[1]}\n{sec_sep}\n{original}\n{up_sep}\n{output2}\n{down_sep}\n\n")
-        else:
+    print(f"{p}/{total}")
+    i = 0
+    while True:
+        if i >= len(instructions):
             empty += 1
-    else:
-        all_outputs.append(f"{i}\nT: {problem[0]}\nN: {problem[1]}\n{sec_sep}\n{original}\n{up_sep}\n{output}\n{down_sep}\n\n")
-    i += 1
-    #os.system("clear")
+            break
+        [original, output] = test_problem(problem, instructions[i])
+        if output != "":
+            all_outputs.append(f"{i}\nT: {problem[0]}\nN: {problem[1]}\n{sec_sep}\n{original}\n{up_sep}\n{output}\n{down_sep}\n\n")
+            break
+        i += 1
+    p += 1
+
+
+    #print(f"{p}/{total}")
+    #[original, output] = test_problem(problem, instructions)
+    #if output == "":
+    #    if instructions2 != "":
+    #        [original, output2] = test_problem(problem, instructions2)
+    #        if output2 == "":
+    #            empty += 1
+    #        else:
+    #            all_outputs.append(f"{i}\nT: {problem[0]}\nN: {problem[1]}\n{sec_sep}\n{original}\n{up_sep}\n{output2}\n{down_sep}\n\n")
+    #    else:
+    #        empty += 1
+    #else:
+    #    all_outputs.append(f"{i}\nT: {problem[0]}\nN: {problem[1]}\n{sec_sep}\n{original}\n{up_sep}\n{output}\n{down_sep}\n\n")
+    #p += 1
+    ##os.system("clear")
 
 empty_string = f"NOT VALID: {empty}/{total} ({round(empty/total*100, 2)}%)"
 all_outputs_string = f"\n{prob_sep}\n".join(all_outputs)
-final_string = f"INSTRUCTIONS1: \"{instructions}\"\nINSTRUCTIONS2: \"{instructions2}\"\n{head_sep}\n{empty_string}\n{head_sep}\n{all_outputs_string}"
+final_string = f"INSTRUCTIONS: {instructions}\n{head_sep}\n{empty_string}\n{head_sep}\n{all_outputs_string}"
 print(final_string)
 open("results/results.txt", "w").write(final_string)
 
